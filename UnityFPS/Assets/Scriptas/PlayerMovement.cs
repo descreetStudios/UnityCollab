22af314185782e82b74b57216844322d39902bc6
descreetStudios <descreetop@gmail.com>
2023-05-14 12:59:44 +0200

    Commit3

--- UnityFPS/Assets/Scriptas/PlayerMovement.cs
+++ UnityFPS/Assets/Scriptas/PlayerMovement.cs
@@ -5,20 +5,44 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public GameObject plr;
+    public CharacterController controller;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.W))
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
         {
-            plr.transform.Translate(0.1f, 0, 0);
+            velocity.y = -2f;
         }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity* Time.deltaTime);
     }
 }
